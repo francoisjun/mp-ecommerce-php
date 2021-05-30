fd260a8ac264def9a9103b5f4d057b6dad19b349
François Júnior <francoisjr@Mac-mini-de-Francois.local>
2021-05-30 19:17:11 -0300

    ajuste no hook

--- hook.php
+++ hook.php
@@ -2,9 +2,9 @@
 $json = file_get_contents('php://input');
 $data = json_decode($json);
 
-$file_name = date("Ymd_His") . "_" . $data->user_id . "_" . $data->data->id;
+$file_name = "retorno"; //date("Ymd_His") . "_" . $data->user_id . "_" . $data->data->id;
 $file = fopen($filename . '.txt', 'w+');
-ftruncate($file, 0);
+//ftruncate($file, 0);
 fwrite($file , $json);
 fclose($file);
 
